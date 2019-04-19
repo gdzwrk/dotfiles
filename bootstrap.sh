31d989d252f776df20296ec6c8e44c1fc07a9d7a
dezwujie <gdzwrk@gmail.com>
2019-04-18 21:41:25 -0500

    Adding first-draft bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ln -s .vimrc ~/.vimrc
+ln -s .bashrc ~/.bashrc
+ln -s .dircolors ~/.dircolors
+ln -s .tmux.conf ~/.tmux.conf
+ln -s .aliases.sh ~/.aliases.sh
+ln -s .inputrc ~/.inputrc
+ln -s .zshrc ~/.zshrc
