b8555a019664e18ffaf9f766f1f9cd617315195f
gdzwrk <gdzwrk@gmail.com>
2019-06-25 12:07:52 -0500

    Including strokesPlus.xml config for mouse gestures

---
+++ gdzGrab.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -x
+#A script to gather various settings, preferences, and scripts that I want to sync between PCs using Google Drive
+
+cp -f /c/Users/garrett.dezeeuw/AppData/Roaming/StrokesPlus/StrokesPlus.xml /c/dev/dotfiles
